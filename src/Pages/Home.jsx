fc8fe49365220453f1e04d8f6182ce6ed362161a
AshishY1920 <ashish.yadav@menrocks.in>
2024-07-08 10:42:28 +0530

    added login & logout functionality

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,31 +1,55 @@
-import React, { Fragment } from 'react'
-import { default as SideBar } from '../Components/Sidebar.common'
-import secureLocalStorage from 'react-secure-storage';
+import React, { Fragment } from "react";
+import { default as SideBar } from "../Components/Sidebar.common";
+import secureLocalStorage from "react-secure-storage";
 import { IoBookmark } from "react-icons/io5";
+import { CiLogout } from "react-icons/ci";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Home = () => {
-    let user = JSON.parse(secureLocalStorage.getItem('user_watch_list'));
-    console.log(user)
-    return (
-        <Fragment>
-            <div className="container">
-                <div className='wrapper-container-grid-row'>
-                    <div>
-                        <SideBar />
-                    </div>
-                    <div>
-                        <h3>Welcome, {user.email.slice(0, 12)}</h3>
+  const navigate = useNavigate();
+  let user = JSON.parse(secureLocalStorage.getItem("user_watch_list"));
+  console.log(user);
 
-                        <div className="watch-list-container">
-                            <h1>Welcome to, <span>Tiny Moviez</span></h1>
-                            <p>Browse movies, add them to watchlists and share them with friends.</p>
-                            <p>Just click the <IoBookmark color='#E76F51' /> to add a movie, the poster to see more details marked the video as watched.</p>
-                        </div>
-                    </div>
-                </div>
+  const handleLogout = () => {
+    secureLocalStorage.removeItem("user_watch_list");
+    navigate("/login");
+    toast.success("Logout Successfully");
+  };
+  return (
+    <Fragment>
+      <div className="container">
+        <div className="wrapper-container-grid-row">
+          <div>
+            <SideBar />
+          </div>
+          <div>
+            <div className="header-info">
+              <h3>Welcome, {user.name}</h3>
+              <Button onClick={() => handleLogout()}>
+                <CiLogout size={28} />
+              </Button>
             </div>
-        </Fragment>
-    )
-}
 
-export default Home
+            <div className="watch-list-container">
+              <h1>
+                Welcome to, <span>Tiny Moviez</span>
+              </h1>
+              <p>
+                Browse movies, add them to watchlists and share them with
+                friends.
+              </p>
+              <p>
+                Just click the <IoBookmark color="#E76F51" /> to add a movie,
+                the poster to see more details marked the video as watched.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Home;
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,55 +1,79 @@
-import React, { Fragment, useState } from 'react'
-import IconRight from '../Assets/Images/Vector.png'
-import IconLeft from '../Assets/Images/VectorLeft.png'
-import { Button } from '@mui/material'
-import Logo from '../Assets/Images/Logo.png'
-import { Link, useNavigate } from 'react-router-dom'
-import { IoIosArrowRoundBack } from "react-icons/io";
-import secureLocalStorage from 'react-secure-storage'
-import { toast } from 'react-toastify'
+import React, { Fragment, useState } from "react";
+import IconRight from "../Assets/Images/Vector.png";
+// import IconLeft from '../Assets/Images/VectorLeft.png'
+import { Button } from "@mui/material";
+import Logo from "../Assets/Images/Logo.png";
+import { Link, useNavigate } from "react-router-dom";
+import { FaArrowLeftLong } from "react-icons/fa6";
+import secureLocalStorage from "react-secure-storage";
+import { toast } from "react-toastify";
+import MetaTitle from "../Components/Meta.name";
 
 const Signup = () => {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState('');
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
 
-    const handleSignup = (e) => {
-        e.preventDefault();
-        if (!email) {
-            toast.error("Email Id is required.")
-            return
-        }
-        const storedUsers = JSON.parse(secureLocalStorage.getItem('usersTinyMoviez')) || [];
-        if (storedUsers.find(user => user.email === email)) {
-            toast.error('Email already exists');
-            return;
-        }
-        const newUsers = [...storedUsers, { email }];
-        secureLocalStorage.setItem('usersTinyMoviez', JSON.stringify(newUsers));
-        toast.success('Signup successful');
-        navigate('/login');
-    };
-    return (
-        <Fragment>
-            <div className="login-container">
-                <img src={IconRight} alt={IconRight} />
-                <img src={IconLeft} alt={IconLeft} />
+  const handleSignup = (e) => {
+    e.preventDefault();
+    if (!email) {
+      toast.error("Email Id is required.");
+      return;
+    }
+    const storedUsers =
+      JSON.parse(secureLocalStorage.getItem("usersTinyMoviez")) || [];
+    if (storedUsers.find((user) => user.email === email)) {
+      toast.error("Email already exists");
+      return;
+    }
+    const newUsers = [...storedUsers, { email: email, name: name }];
+    secureLocalStorage.setItem("usersTinyMoviez", JSON.stringify(newUsers));
+    toast.success("Signup successful");
+    navigate("/login");
+  };
+  return (
+    <Fragment>
+      <MetaTitle title={`Tiny Moviez - Signup`} />
+      <div className="login-container">
+        <img src={IconRight} alt={IconRight} />
+        {/* <img src={IconLeft} alt={IconLeft} /> */}
 
-                <form onSubmit={handleSignup}>
-                    <div>
-                        <img src={Logo} alt={Logo} />
-                    </div>
-                    <div onClick={() => navigate(-1)}>
-                        <IoIosArrowRoundBack />
-                        <p>Go Back</p>
-                    </div>
-                    <label htmlFor="email">Email</label><br />
-                    <input type="text" name="email" id="email" value={email} placeholder='Enter Your Email' onChange={(e) => setEmail(e.target.value)} />
-                    <p>Already Have an Account ? <Link to={`/login`}>Login</Link></p>
-                    <Button type='submit'>SIGNUP</Button>
-                </form>
-            </div>
-        </Fragment>
-    )
-}
+        <form onSubmit={handleSignup}>
+          <div>
+            <img src={Logo} alt={Logo} />
+          </div>
+          <div onClick={() => navigate(-1)}>
+            <FaArrowLeftLong />
+            <p>Go Back</p>
+          </div>
+          <label htmlFor="name">Name</label>
+          <br />
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={name}
+            placeholder="Enter Your Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label htmlFor="email">Email</label>
+          <br />
+          <input
+            type="text"
+            name="email"
+            id="email"
+            value={email}
+            placeholder="Enter Your Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <p>
+            Already Have an Account ? <Link to={`/login`}>Login</Link>
+          </p>
+          <Button type="submit">SIGNUP</Button>
+        </form>
+      </div>
+    </Fragment>
+  );
+};
 
-export default Signup
+export default Signup;
